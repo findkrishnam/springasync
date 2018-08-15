ba0cb1956faf496c49a42c04336d2f4760a85bb3
Krishnam G <findkrishnam@gmail.com>
2018-08-15 21:49:15 +0530

    initial commit of project files

---
+++ src/test/java/com/krishnamg/async/springasync/SpringasyncApplicationTests.java
@@ -0,0 +1,16 @@
+package com.krishnamg.async.springasync;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringasyncApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

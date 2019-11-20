452422e984d48dd34d7813736b1be04d99c61a5c
krishnamg <findkrishnam@gmail.com>
2019-11-20 18:07:48 +0530

    added sonarqube + checkstyle integration in pom.xml

--- src/main/java/com/krishnamg/async/service/GithubLookupService.java
+++ src/main/java/com/krishnamg/async/service/GithubLookupService.java
@@ -3,7 +3,6 @@ package com.krishnamg.async.service;
 import com.krishnamg.async.dto.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
